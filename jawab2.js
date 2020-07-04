679ecd3f7915d820094434829e0587cd3b8c4af2
Wahyu0994 <60283357+Wahyu0994@users.noreply.github.com>
2020-07-04 21:56:07 +0700

    jawab no.2

---
+++ jawab2.js
@@ -0,0 +1,21 @@
+<script>
+
+var key = 'tasty food';
+
+var value = ['foo','bar'],
+    length = value.length;
+    function ceck(key,alue) {
+while(length--) {
+   if (yourstring.indexOf(substrings[length])!=-1) {
+       document.write('true');
+   }
+   else{
+    document.write ('false');
+   }
+   }
+   
+   }
+   
+   ceck (key,value);
+
+</script>
